8e5c3e3d47b7b13f78a59addd96d4bec10cf6787
TIENSIHUNG <binlung44qn@gmail.com>
2023-10-11 19:54:00 +0700

    up

--- S3/insert_Arr/src/main.java
+++ S3/insert_Arr/src/main.java
@@ -1,23 +1,21 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] arr = {1,2,3,4,5,6,7,8};
+        System.out.println(Arrays.toString(arr));
         System.out.println("nhap so muon chen");
         int x = Integer.parseInt(sc.nextLine());
         System.out.println("nhap index");
         int z = Integer.parseInt(sc.nextLine());
-        for (int i = 0; i <= arr.length; i++) {
-            if (i == z){
-                arr[i] = x;
-                for (int j = arr.length; j > i ; j--) {
-                    arr[j] = arr[j-1];
-                }
-            }
-        }
+        ArrayList arrayList = new ArrayList<>();
         for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i]);
+            arrayList.add(arr[i]);
         }
+        arrayList.add(z,x);
+        System.out.println(arrayList);
     }
 }
